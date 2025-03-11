ab15d8d936a028a54bf63dfc75f1fd50eb2d9b0f
Maksym Nikitenko <famahana55@gmail.com>
2025-03-11 18:42:56 +0100

    Done

---
+++ Store/CreditCardPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class CreditCardPayment : IPaymentMethod
+    {
+        public string CardNumber { get; set; }
+
+        public CreditCardPayment(string cardNumber) => CardNumber = cardNumber;
+
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Оплата {amount} руб. произведена с карты {CardNumber}");
+        }
+    }
+}
---
+++ Store/PayPalPayment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class PayPalPayment : IPaymentMethod
+    {
+        public string Email { get; set; }
+
+        public PayPalPayment(string email) => Email = email;
+    }
+}
