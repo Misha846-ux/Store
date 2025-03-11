5029db0f990f895fcf09a42a75320715ef0d3deb
RuslanOpihaylenko <rysya08shkolnik@gmail.com>
2025-03-11 19:41:54 +0200

    first commit

---
+++ Store/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; } // Агрегация
+        public Supplier Supplier { get; set; } // Композиция
+
+        public Product(string name, decimal price, Category category, Supplier supplier)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            Supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {Category.Name}, Цена: {Price} руб.");
+            Console.WriteLine($"Поставщик: {Supplier.Name}");
+            Category.DisplayCategoryInfo();
+        }
+    }
+}
